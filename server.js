9a57d1f23f93bfd98258576862e1d9c7c469368d
ntibbals <ntibbals@outlook.com>
2018-12-03 15:19:16 -0800

    redirected server

--- server.js
+++ server.js
@@ -5,11 +5,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static('./public'));
-
 app.get('/home', function(req, res){
   res.sendFile(`${__dirname}/public/index.html`);
 });
-
 app.listen(PORT, function() {
   console.log('listening on port', PORT);
 });
