3b960bdc4e17b2a12756aed1bbd8f209d2a1f495
ntibbals <ntibbals@outlook.com>
2018-12-03 15:16:49 -0800

    changes

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ app.use(express.static('./'));
 app.get('/home', function(req, res){
   res.sendFile(`${__dirname}/public/index.html`);
 });
-
 app.listen(PORT, function() {
   console.log('listening on port', PORT);
 });
